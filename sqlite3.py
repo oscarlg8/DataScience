86f2fb7779eb591e110615cdd5b386133b3c585f
Oscar Lopez g <76225808+oscarlg8@users.noreply.github.com>
2023-03-15 16:39:21 -0500

    gta

--- sqlite3.py
+++ sqlite3.py
@@ -1,14 +1,11 @@
-import sqlite3 as sql
+release_list = [
+    (1997, "Grand Theft Auto", "state of New Guernsey"),
+    (1999, "Grand Theft Auto 2", "Anywhere, USA"),
+    (2001, "Grand Theft Auto III", "Liberty City"),
+    (2002, "Grand Theft Auto: Vice City", "Vice City"),
+    (2004, "Grand Theft Auto: San Andreas", "state of San Andreas"),
+    (2008, "Grand Theft Auto IV", "Liberty City"),
+    (2013, "Grand Theft Auto V", "Los Santos")
+]    
 
-
-def createDB():
-    conn = sql.connect('test.db')
-    print("Opened database successfully")
-    conn.execute('''CREATE TABLE COMPANY
-           (ID INT PRIMARY KEY     NOT NULL,
-           NAME           TEXT    NOT NULL,
-           AGE            INT     NOT NULL,
-           ADDRESS        CHAR(50),
-           SALARY         REAL);''')
-    print("Table created successfully")
-    conn.close()
+print(release_list)
